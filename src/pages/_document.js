86f6ef87afdb185fcfaa41847eea17815974258b
INNOVATIVE LEARNING HORIZONS <innovativelearninghorizons@gmail.com>
2025-11-05 15:45:23 +0530

    added tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -28,6 +28,20 @@ export default function Document() {
         <script src="https://analytics.ahrefs.com/analytics.js" data-key="6GsYocIP1Z05nJKLoVupGA" async></script>
       
 
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WCKV5V2S');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Google Analytics Tag */}
         <script
           async
@@ -127,6 +141,17 @@ export default function Document() {
       </Head>
 
       <body>
+{/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WCKV5V2S"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Main />
         <NextScript />
       </body>
