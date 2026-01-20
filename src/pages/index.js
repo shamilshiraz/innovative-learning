887bff6256af8e4b97604588cfc341c72a1425e7
Vinothkumar. S <vin.cse80@gmail.com>
2026-01-20 14:48:36 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import WelcomeSection from '@/components/Intro'
 import LandingSection from '@/components/Landing'
 import Navbar from '@/components/Navbar'
 import WhyChooseUs from '@/components/Why'
-import WhyChooseUs from '@/components/Testimonials'
+import Testimonials from '@/components/Testimonials'
 import React from 'react'
 import Head from "next/head";
 
